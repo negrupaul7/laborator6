fd3d3cefd354cc5fd5ddeada41be639e8c50fe69
paul <negrupaul7@gmail.com>
2017-12-01 00:45:24 +0200

    exercitiul2

--- main.cpp
+++ main.cpp
@@ -1,30 +1,29 @@
-#include <iostream>
+long factorial (int n);
+double suma(int n, double a[]);
 
-using namespace std;
-
-  // declaram un sir de valori intregi si doi vectori v1 si v2
-
-
-
-struct vector
+int main() // cele doua functii care contin apeluri recursive factorial si suma
 {
-    double x, y, z;
-};
+    long f;
+    double s;
+    double a[5] = {10., 33., -12., 1., 10.};
+    f=factorial(6);
+    s=suma(5, a);
+    return 0;
+}
 
-int suma(int a[], int n);
-void modul(int a[], int n);
-void permcirc (int s[], int n);
-vector sumv(vector a, vector b);
-double prods(vector a,vector b);
-vector versor(vector a);
-
-int main()
+long factorial(int n)
 {
-  int v [] = {2, 5, -19, 34, 7, -12};
-  vector v1, v2;
-  v1.x = 12.; v1.y = 7.; v1.z = -1.;
-  v2.x = -1.; v2.y = 5.; v2.z = 0.;
-  int sm = suma(v, 6);
-  vector sv = sumv(v1, v2);
-  return 0;
+
+    if(n == 1)
+        return 1;
+    else
+        return n * factorial (n-1);
+}
+
+double suma(int n,double a[])
+{
+    if(n == 1)
+        return a[0];
+    else
+        return a[n-1] + suma(n-1, a);
 }
